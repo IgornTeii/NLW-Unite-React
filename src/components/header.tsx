555397cc0fab55df67df863a7848bceb1ea3810f
Igor <igornteixeira48@gmail.com>
2024-04-10 11:35:52 -0300

    feature: attendeelist

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,17 @@
-import nlwUniteIcon from '../assets/nlw-unite-icon.svg'
+import nlwUniteIcon from "../assets/nlw-unite-icon.svg";
 
 export function Header() {
-  return(
-    <div className='flex items-center gap-5 p-2'>
-        <img src={nlwUniteIcon} alt="Icon NLW" />
-        <nav className='flex items-center gap-5'>
-            <a href="" className='font-medium text-sm text-zinc-300'>Eventos</a>
-            <a href="" className='font-medium text-sm'>Participantes</a>
-        </nav>
+  return (
+    <div className="flex items-center gap-5 p-2">
+      <img src={nlwUniteIcon} alt="Icon NLW" />
+      <nav className="flex items-center gap-5">
+        <a href="" className="font-medium text-sm text-zinc-300">
+          Eventos
+        </a>
+        <a href="" className="font-medium text-sm">
+          Participantes
+        </a>
+      </nav>
     </div>
-  )
+  );
 }
