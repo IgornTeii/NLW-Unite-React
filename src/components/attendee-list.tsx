aa0669ba681dd909dad7ba18d0f015fb65733c2e
Igor <igornteixeira48@gmail.com>
2024-04-10 10:25:30 -0300

    feature: attendeelist

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -2,7 +2,7 @@ import { Search } from "lucide-react";
 
 export function AttendeeList() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
         <h1 className="text-2xl font-bold">Participantes</h1>
         <div className="p-3 w-72 py-1.5 border border-white/10 bg-transparent rounded-lg text-sm flex items-center gap-3">
@@ -13,47 +13,49 @@ export function AttendeeList() {
           ></input>
         </div>
       </div>
-      <table>
+      <div className="border border-white/10 rounded-lg">
+      <table className="w-full">
         <thead>
-          <tr>
-            <th>
+          <tr className="border-b border-white/10">
+            <th className="py-3 px-4 font-semibold text-sm">
               <input type="checkbox" />
             </th>
-            <th>Código</th>
-            <th>Participantes</th>
-            <th>Data de inscrição</th>
-            <th>Data do check-in </th>
-            <th></th>
+            <th className="py-3 px-4 font-semibold text-sm text-left">Código</th>
+            <th className="py-3 px-4 font-semibold text-sm text-left">Participantes</th>
+            <th className="py-3 px-4 font-semibold text-sm text-left">Data de inscrição</th>
+            <th className="py-3 px-4 font-semibold text-sm text-left">Data do check-in </th>
+            <th className="py-3 px-4 font-semibold text-sm text-left"></th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
+          <tr className="border-b border-white/10">
+            <td className="py-3 px-4 text-sm text-zinc-300">
               <input type="checkbox" />
             </td>
-            <td>551099</td>
-            <td>
-                <div>
-                    <span>Igor Nunes Teixeira</span>
+            <td className="py-3 px-4 text-sm text-zinc-300">551099</td>
+            <td className="py-3 px-4 text-sm text-zinc-300">
+                <div className="flex flex-col gap-1">
+                    <span className="font-semibold text-white">Igor Nunes Teixeira</span>
                     <span>igornteixeira48@hotmail.com</span>
                 </div>
             </td>
-            <td>7 dias atrás</td>
-            <td>3 dias atrás</td>
-            <td></td>
+            <td className="py-3 px-4 text-sm text-zinc-300">7 dias atrás</td>
+            <td className="py-3 px-4 text-sm text-zinc-300">3 dias atrás</td>
+            <td className="py-3 px-4 text-sm text-zinc-300"></td>
           </tr>
         </tbody>
         <tfoot>
             <tr>
-                <td colSpan={3}>
+                <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
                     Mostrando 10 de 228 itens 
                 </td>
-                <td colSpan={3}>
+                <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
                     Página 1 de 23
                 </td>
             </tr>
         </tfoot>
       </table>
+    </div>
     </div>
   );
 }
