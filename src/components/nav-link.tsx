9f462938148eac94f138f3564a21d8c173bef1a7
Igor <igornteixeira48@gmail.com>
2024-04-18 18:35:01 -0300

    feature: props

---
+++ src/components/nav-link.tsx
@@ -0,0 +1,12 @@
+interface NavLinkProps{
+    children: string
+    href: string
+}
+
+export function NavLink(props: NavLinkProps) {
+    return(
+        <a href={props.href} className="font-medium text-sm">
+            {props.children}
+        </a>
+    )
+}
