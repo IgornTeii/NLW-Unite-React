214f18151d17720520119889c5d39b80f0cc202e
Igor <igornteixeira48@gmail.com>
2024-04-19 10:21:16 -0300

    feature: tablecell

--- src/components/table/table-header.tsx
+++ src/components/table/table-header.tsx
@@ -1,9 +1,9 @@
 import { ComponentProps } from "react";
 
-interface TableHeaderProps extends ComponentProps<'th'>{}
+interface TableHeaderProps extends ComponentProps<"th"> {}
 
 export function TableHeader(props: TableHeaderProps) {
-    return(
-        <th {...props} className="py-3 px-4 text-sm font-semibold text-left"></th>
-    )
+  return (
+    <th {...props} className="py-3 px-4 text-sm font-semibold text-left" />
+  );
 }
