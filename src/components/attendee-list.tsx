43fad879750795bd0ed0c0cc505d01383a325bf1
Igor <igornteixeira48@gmail.com>
2024-04-25 14:08:55 -0300

    feature: push dates

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -6,13 +6,23 @@ import {
   ChevronsRight,
   ChevronRight,
 } from "lucide-react";
+import { formatRelative } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { IconButton } from "./icon-button";
 import { Table } from "./table/table";
 import { TableHeader } from "./table/table-header";
 import { TableCell } from "./table/table-cell";
 import { TableRow } from "./table/table-row";
+import { ChangeEvent, useState } from "react";
+import { attendees } from "../data/attendees";
 
 export function AttendeeList() {
+  const [search, setSearch] = useState("");
+
+  function onSearchImputChange(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
@@ -22,64 +32,51 @@ export function AttendeeList() {
           <input
             className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"
             placeholder="Buscar participante..."
+            onChange={onSearchImputChange}
           />
+
+          {search}
         </div>
       </div>
 
       <Table>
         <thead>
           <tr className="border-b border-white/10">
-            <TableHeader
-              style={{ width: 48 }}
-            >
+            <TableHeader style={{ width: 48 }}>
               <input
                 type="checkbox"
                 className="size-4 bg-black/20 rounded border border-white/10"
               />
             </TableHeader>
-            <TableHeader>
-              Código
-            </TableHeader>
-            <TableHeader>
-              Participantes
-            </TableHeader>
-            <TableHeader>
-              Data de inscrição
-            </TableHeader>
-            <TableHeader>
-              Data do check-in
-            </TableHeader>
-            <TableHeader
-              style={{ width: 64 }}
-            ></TableHeader>
-          </tr>  
+            <TableHeader>Código</TableHeader>
+            <TableHeader>Participantes</TableHeader>
+            <TableHeader>Data de inscrição</TableHeader>
+            <TableHeader>Data do check-in</TableHeader>
+            <TableHeader style={{ width: 64 }}></TableHeader>
+          </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 8 }).map((_, i) => {
+          {attendees.map((attendees) => {
             return (
-              <TableRow key={i}>
-                <TableCell className="py-3 px-4 text-sm text-zinc-300">
+              <TableRow key={attendees.id}>
+                <TableCell>
                   <input
                     type="checkbox"
                     className="size-4 bg-black/20 rounded border border-white/10"
                   />
                 </TableCell>
-                <TableCell className="py-3 px-4 text-sm text-zinc-300">551099</TableCell>
-                <TableCell className="py-3 px-4 text-sm text-zinc-300">
+                <TableCell>{attendees.id}</TableCell>
+                <TableCell>
                   <div className="flex flex-col gap-1">
                     <span className="font-semibold text-white">
-                      Igor Nunes Teixeira
+                      {attendees.name}
                     </span>
-                    <span>igornteixeira48@hotmail.com</span>
+                    <span>{attendees.email}</span>
                   </div>
                 </TableCell>
-                <TableCell className="py-3 px-4 text-sm text-zinc-300">
-                  7 dias atrás
-                </TableCell>
-                <TableCell className="py-3 px-4 text-sm text-zinc-300">
-                  3 dias atrás
-                </TableCell>
-                <TableCell className="py-3 px-4 text-sm text-zinc-300">
+                <TableCell>{formatRelative(attendees.createdAt, new Date(), {locale: ptBR})}</TableCell>
+                <TableCell>{formatRelative(attendees.createdInAt, new Date(), {locale: ptBR})}</TableCell>
+                <TableCell>
                   <IconButton transparent>
                     <MoreHorizontal className="size-4" />
                   </IconButton>
@@ -90,13 +87,8 @@ export function AttendeeList() {
         </tbody>
         <tfoot>
           <tr>
-            <TableCell className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
-              Mostrando 10 de 228 itens
-            </TableCell>
-            <TableCell
-              className="py-3 px-4 text-sm text-zinc-300 text-right"
-              colSpan={3}
-            >
+            <TableCell colSpan={3}>Mostrando 10 de 228 itens</TableCell>
+            <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
                 <span>Página 1 de 23</span>
                 <div className="flex gap-1.5">
